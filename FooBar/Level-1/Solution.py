bfc5b1e09e49ea954ae56c7f8bc45c44eec50df7
VARUN TOTAKURA <totakura.varun@gmail.com>
2020-02-09 11:13:44 +0530

    Commit

---
+++ FooBar/Level-1/Solution.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def answer(data, n):
+    if n == 0:
+        return []
+    if len(data) < n:
+        return data
+    c = Counter(data)
+    return [d for d in data if c[d] <= n]
+
+print(answer([1, 2, 2, 3, 3, 3, 4, 5, 5],1))
